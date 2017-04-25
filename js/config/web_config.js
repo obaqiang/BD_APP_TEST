518b7b1a14e7a163046a95744b9cc1d31a5cf8e8
qiangqiangbuzhidao <qiangqiangbuzhidao@DESKTOP-I8LFEUJ>
2017-04-25 17:37:59 +0800

    2017/4/25

---
+++ js/config/web_config.js
@@ -0,0 +1,8 @@
+//var test_url='http://192.168.1.163';
+
+//var yehongbing_url='http://192.168.1.192';
+var yehongbing_url = 'http://192.168.1.170:82';
+//var yehongbing_url = 'http://testapi.bdvip.net';
+//var test_url = 'http://testapi.bdvip.net';
+//var test_url = 'http://192.168.1.192';
+var test_url = 'http://192.168.1.170:82';
