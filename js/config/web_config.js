38c4d4f973d9de35afa9c689f76529a613734574
qiangqiangbuzhidao <qiangqiangbuzhidao@DESKTOP-I8LFEUJ>
2017-04-28 14:36:05 +0800

    2017/4/28

--- js/config/web_config.js
+++ js/config/web_config.js
@@ -1,8 +1,14 @@
-//var test_url='http://192.168.1.163';
 
-//var yehongbing_url='http://192.168.1.192';
+
+
+//本地
 var yehongbing_url = 'http://192.168.1.170:82';
+var test_url = 'http://192.168.1.170:82';
+
+//测试
 //var yehongbing_url = 'http://testapi.bdvip.net';
 //var test_url = 'http://testapi.bdvip.net';
-//var test_url = 'http://192.168.1.192';
-var test_url = 'http://192.168.1.170:82';
+
+//正式
+//var yehongbing_url = 'https://api.bdvip.net';
+//var test_url = 'https://api.bdvip.net';
