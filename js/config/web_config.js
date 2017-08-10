4577467cb846722d1ecfc89eae16453e1770f4a1
qiangqiangbuzhidao <qiangqiangbuzhidao@DESKTOP-I8LFEUJ>
2017-08-10 15:25:25 +0800

    2017/8/10

--- js/config/web_config.js
+++ js/config/web_config.js
@@ -2,13 +2,16 @@
 
 
 //本地
-var yehongbing_url = 'http://192.168.1.170:82';
-var test_url = 'http://192.168.1.170:82';
+//var yehongbing_url = 'http://192.168.1.170:82';
+//var test_url = 'http://192.168.1.170:82';
 
 //测试
 //var yehongbing_url = 'http://testapi.bdvip.net';
 //var test_url = 'http://testapi.bdvip.net';
 
+//测试_2
+var yehongbing_url = 'http://testapi.ks12580.net';
+var test_url = 'http://testapi.ks12580.net';
 //正式
 //var yehongbing_url = 'https://api.bdvip.net';
 //var test_url = 'https://api.bdvip.net';
