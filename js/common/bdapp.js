a51f7648933a48d7a676306d2dac70c4fc6ecd26
qiangqiangbuzhidao <qiangqiangbuzhidao@DESKTOP-I8LFEUJ>
2017-08-30 13:49:01 +0800

    2017/8/30

--- js/common/bdapp.js
+++ js/common/bdapp.js
@@ -389,12 +389,12 @@ function js_date_time(unixtime) {
 
 function getNowFormatDate(newDate) {
 	var date = new Date();
-    date.setTime(newDate * 1000);
+	date.setTime(newDate * 1000);
 	var seperator1 = "-";
 	var seperator2 = ":";
 	var month = date.getMonth() + 1;
 	var strDate = date.getDate();
-	
+
 	if(month >= 1 && month <= 9) {
 		month = "0" + month;
 	}
@@ -404,20 +404,24 @@ function getNowFormatDate(newDate) {
 	}
 	console.log(strDate);
 	var hours = date.getHours();
-	if(hours<10){
-		hours = '0'+hours;
+	if(hours < 10) {
+		hours = '0' + hours;
 	}
 	var minutes = date.getMinutes();
-	if(minutes<10){
-		minutes = '0'+minutes;
+	if(minutes < 10) {
+		minutes = '0' + minutes;
 	}
 	var seconds = date.getSeconds();
-	if(seconds<10){
-		seconds = '0'+seconds;
+	if(seconds < 10) {
+		seconds = '0' + seconds;
 	}
 	console.log(date.getMinutes());
 	var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
 		" " + hours + seperator2 + minutes +
 		seperator2 + seconds;
 	return currentdate;
+}
+
+function goBack() {
+	history.go(-1)
 }
